e62f06b3517d23488dbc9135927110d730d21d92
Rahul Joshi <rahul.joshi1@thoughtworks.com>
2021-08-31 19:59:38 +0530

    Formats timestamp to display when comment posted

--- src/modules/comments/CommentCard.js
+++ src/modules/comments/CommentCard.js
@@ -52,6 +52,45 @@ const ReplyInput = styled(CommentInput)`
   margin-top: 10px;
 `;
 
+const HOURS_IN_DAY = 24;
+const MINUTES_IN_HOUR = 60;
+const SECONDS_IN_MINUTE = 60;
+const MILLISECONDS_IN_SECOND = 1000;
+
+const DateUtils = {
+  toDays(seconds) {
+    return Math.floor(
+      seconds / (HOURS_IN_DAY * MINUTES_IN_HOUR * SECONDS_IN_MINUTE)
+    );
+  },
+  toHours(seconds) {
+    return Math.floor(seconds / (MINUTES_IN_HOUR * SECONDS_IN_MINUTE));
+  },
+  toMinutes(seconds) {
+    return Math.floor(seconds / SECONDS_IN_MINUTE);
+  },
+  toSeconds(milliseconds) {
+    return milliseconds / MILLISECONDS_IN_SECOND;
+  },
+};
+
+function formatTimePassed(timestamp) {
+  const currentTime = Date.now();
+
+  const differentInSeconds = DateUtils.toSeconds(currentTime - timestamp);
+
+  const days = DateUtils.toDays(differentInSeconds);
+  if (days > 0) return `${days} d`;
+
+  const hours = DateUtils.toHours(differentInSeconds);
+  if (hours > 0) return `${hours} h`;
+
+  const minutes = DateUtils.toMinutes(differentInSeconds);
+  if (minutes > 0) return `${minutes} m`;
+
+  return "a few seconds ago";
+}
+
 function ActionFooter({ timestamp, onReply }) {
   return (
     <Ul>
@@ -62,7 +101,7 @@ function ActionFooter({ timestamp, onReply }) {
         <LinkButton onClick={onReply}>Reply</LinkButton>
       </Li>
       <Li>
-        <TimeStamp>{timestamp}</TimeStamp>
+        <TimeStamp>{formatTimePassed(timestamp)}</TimeStamp>
       </Li>
     </Ul>
   );
